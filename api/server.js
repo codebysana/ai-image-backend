385c6f2c6d87a3ea7d0e50b319e36640a499b18e
Sana Younas <179322071+codebysana@users.noreply.github.com>
2025-12-11 02:16:27 +0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -6,4 +6,9 @@ const { app, connectToDatabase } = require('../app');
 // Attempt DB connection on cold start to reduce latency on first request
 connectToDatabase().catch((err) => console.error('DB connect failed', err));
 
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is running!" });
+});
+
 module.exports = serverless(app);
+
